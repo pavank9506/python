c66e444fe764d47a24a0bb805fda4cfcba29fb00
Kota Ganesh <pavan.kota439@gmail.com>
2025-04-29 16:12:43 +0530

    pyton file added

--- hello.py
+++ hello.py
@@ -8,3 +8,6 @@ print(result)
 print("memory ID :",id(result))
 print("data type :",type(result))
 print(dir(result))
+sample_str = "this is a string"
+print(sample_str, type(sample_str))
+print(dir(sample_str))
