67ccc12b978757cd81303723a55a146f66fec0fc
Kota Ganesh <pavan.kota439@gmail.com>
2025-05-01 01:14:09 +0530

    pyton file added

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-print("hello world")
+""" print("hello world")
 print(10+20)
 a=20
 b=30
@@ -9,28 +9,28 @@ print("memory ID :",id(result))
 print("data type :",type(result))
 print(dir(result))
 sample_str = "this is a string"
-print(sample_str, type(sample_str))
+print(sample_str, type(sample_str) """)
 # print(dir(sample_str))
 
 # 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
 
-print(sample_str.capitalize())  # converts first letter into capital
+""" print(sample_str.capitalize())  # converts first letter into capital
 
 print(sample_str.casefold())    #converts everything iinto small case
 # print(sample_str.center())
-print(sample_str.split(' '))   # convert a string into words using space as delimeter
+print(sample_str.split(' ')) """   # convert a string into words using space as delimeter
 
 
-sample_str = "Hello,name is pavan"
+""" sample_str = "Hello,name is pavan"
 
 print(sample_str.index('H'))
-print(sample_str.index('o'))
+print(sample_str.index('o')) """
 
 # python supports negative indexing
 
-sample_char = sample_str[-1]
+""" sample_char = sample_str[-1]
 print(sample_char)
-print(sample_str.find('p'))
+print(sample_str.find('p')) """
 
 
 
@@ -55,6 +55,6 @@ print(dir(l)) """
 
 l = [1,2,3,"abc",True,[1,2,3,"abc",True]]
 
-l.append(true)
+l.append(True)
 
 print(l)
