febac23b593e63c242617ac8dfd11860ca78a8e8
Kota Ganesh <pavan.kota439@gmail.com>
2025-05-01 01:17:11 +0530

    pyton file added

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ print("memory ID :",id(result))
 print("data type :",type(result))
 print(dir(result))
 sample_str = "this is a string"
-print(sample_str, type(sample_str) """)
+print(sample_str, type(sample_str))"""
 # print(dir(sample_str))
 
 # 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
